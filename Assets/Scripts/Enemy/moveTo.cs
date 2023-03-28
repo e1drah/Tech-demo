490a5cbc8f629c2b87857b59e72b1c0f428fcd14
Jacob Hardie <Hardiejacob22@gmail.com>
2023-03-28 09:41:21 -0300

    Finished Ai code

--- Assets/Scripts/Enemy/moveTo.cs
+++ Assets/Scripts/Enemy/moveTo.cs
@@ -10,15 +10,23 @@ public class moveTo : MonoBehaviour
     public Transform point2;
     public Transform point3;
     public Transform point4;
+
     public GameObject player;
+    public GameObject eye;
+
+    public float detectionRange;
+    public float outDetectionRange;
+    public float attackRange = 1.5f;
 
     private Vector3 playerLastPostion;
+    private Vector3 nextPoint;
     private enum STATE
     {
         PATROL,
         FOLLOW,
         SEARCH,
-        RETREAT
+        RETREAT,
+        ATTACK
     }
     //private int STATE_PATROLLING = 1;
     //private int STATE_FOLLOWING = 2;
@@ -32,78 +40,108 @@ public class moveTo : MonoBehaviour
     {
         agent = gameObject.GetComponent<NavMeshAgent>();
         agent.destination = point1.position;
-        state = (int) STATE.PATROL;
+        nextPoint = point1.position;
+        state = (int)STATE.PATROL;
     }
     private void Update()
     {
-        if ((player.transform.position.x - gameObject.transform.position.x <= 10) && (player.transform.position.y - gameObject.transform.position.y <= 10))
+        if (RangeChecker(detectionRange))
         {
-            state = (int) STATE.FOLLOW;
+            state = (int)STATE.FOLLOW;
         }
-        if ((player.transform.position.x - gameObject.transform.position.x >= 10) && (player.transform.position.y - gameObject.transform.position.y >= 10))
+        if ( OutRangeChecker(outDetectionRange) && (state == (int)STATE.FOLLOW))
         {
-            state = (int) STATE.SEARCH;
+            state = (int)STATE.SEARCH;
         }
-        if ((gameObject.transform.position == playerLastPostion) && (state == (int) STATE.SEARCH))
+        if (((playerLastPostion.x == gameObject.transform.position.x) && (playerLastPostion.z == gameObject.transform.position.z)) && (state == (int)STATE.SEARCH))
         {
-            state = (int) STATE.RETREAT;
+            state = (int)STATE.RETREAT;
         }
-        if ((state == (int) STATE.RETREAT) && (gameObject.transform.position == point1.position))
+        if ((state == (int)STATE.RETREAT) && (gameObject.transform.position == point1.position))
         {
-            state = (int) STATE.PATROL;
+            state = (int)STATE.PATROL;
+        }
+        if (RangeChecker(attackRange))
+        {
+            state = (int)STATE.ATTACK;
         }
         StateUpdate();
     }
     public void StateUpdate()
     {
-        if (state == (int) STATE.PATROL)
+        if (state == (int)STATE.PATROL)
         {
             Patrol();
         }
-        if (state == (int) STATE.FOLLOW)
+        if (state == (int)STATE.FOLLOW)
         {
             agent.destination = player.transform.position;
             playerLastPostion = player.transform.position;
+            eye.GetComponent<Renderer>().material.color = Color.red;
+            gameObject.GetComponentInChildren<Light>().color = Color.red;
         }
-        if (state == (int) STATE.SEARCH)
+        if (state == (int)STATE.SEARCH)
         {
             agent.destination = playerLastPostion;
+            eye.GetComponent<Renderer>().material.color = Color.yellow;
+            gameObject.GetComponentInChildren<Light>().color = Color.yellow;
         }
-        if (state == (int) STATE.RETREAT)
+        if (state == (int)STATE.RETREAT)
         {
-            agent.destination = point1.position;
+            agent.destination = nextPoint;
+            eye.GetComponent<Renderer>().material.color = Color.white;
+            gameObject.GetComponentInChildren<Light>().color = Color.white;
+            state = (int)STATE.PATROL;
+        }
+        if (state == (int)STATE.ATTACK)
+        {
+            eye.GetComponent<Renderer>().material.color = Color.magenta;
+            agent.destination = gameObject.transform.position;
+            gameObject.GetComponentInChildren<Light>().color = Color.magenta;
         }
     }
-    public void Patrol()
-    {
-        if (gameObject.transform.position == point1.position)
+    void Patrol()
         {
-            Debug.Log("point 1 reached");
-            agent.destination = point2.position;
+            if (gameObject.transform.position == point1.position)
+            {
+                Debug.Log("point 1 reached");
+                agent.destination = point2.position;
+                nextPoint = point2.position;
+            }
+            if (gameObject.transform.position == point2.position)
+            {
+                Debug.Log("point 2 reached");
+                agent.destination = point3.position;
+                nextPoint = point2.position;
+            }
+            if (gameObject.transform.position == point3.position)
+            {
+                Debug.Log("point 3 reached");
+                agent.destination = point4.position;
+                nextPoint = point4.position;
+            }
+            if (gameObject.transform.position == point4.position)
+            {
+                Debug.Log("point 4 reached");
+                agent.destination = point1.position;
+                nextPoint = point1.position;
+            }
         }
-        if (gameObject.transform.position == point2.position)
-        {
-            Debug.Log("point 2 reached");
-            agent.destination = point3.position;
-        }
-        if (gameObject.transform.position == point3.position)
-        {
-            Debug.Log("point 3 reached");
-            agent.destination = point4.position;
-        }
-        if (gameObject.transform.position == point4.position)
-        {
-            Debug.Log("point 4 reached");
-            agent.destination = point1.position;
-        }
-    }
 
-    //public void OnTriggerEnter(Collider other)
-    //{
-    //    state = STATE_FOLLOWING;
-    //}
-    //public void OnTriggerExit(Collider other)
-    //{
-    //    state = STATE_RETREET;
-    //}
+    public bool RangeChecker(float range)
+    {
+        if((player.transform.position.x - gameObject.transform.position.x <= range) && (player.transform.position.z - gameObject.transform.position.z <= range) && ((player.transform.position.x - gameObject.transform.position.x >= -range) && (player.transform.position.z - gameObject.transform.position.z >= -detectionRange)))
+        {
+            return true;
+        }
+        return false;
+    }
+    public bool OutRangeChecker(float range)
+    {
+        if (((player.transform.position.x - gameObject.transform.position.x > range) || (player.transform.position.z - gameObject.transform.position.z > range)) || ((player.transform.position.x - gameObject.transform.position.x < -range) || (player.transform.position.z - gameObject.transform.position.z < -range)))
+        {
+            return true;
+        }
+        return false;
+    }
 }
